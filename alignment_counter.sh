cb725b917287922731571c438613415db32df0eb
B180794-2020 <email_not_provided>
2020-10-21 18:24:00 +0100

    Working quality checker

--- alignment_counter.sh
+++ alignment_counter.sh
@@ -5,12 +5,43 @@
 #Pathing to list of sequence files  & looping rows containing each pair 
 fqDir="/localdisk/data/BPSM/Assignment1/fastq"
 fqTable="/localdisk/data/BPSM/Assignment1/fastq/fqfiles"
-while IFS= read -r  line
-	do
-	seq_1=$(echo $line | cut -d" " -f3)
-	seq_2=$(echo $line | cut -d" " -f4)
-	fastqc -t 2  $fqDir/$seq_1 $fqDir/$seq_2 -o ~/Assignment1
-	done < "$fqTable"
+
+while IFS= read -r  line; do
+#extracting file names for each pair in pair-sequence alignment & running quality check using fastqc
+	file_1=$(echo $line | cut -d" " -f3 | cut -d "." -f1)
+	file_2=$(echo $line | cut -d" " -f4 | cut -d "." -f1)
+	fastqc --extract  -t 2  $fqDir/$file_1.fq.gz $fqDir/$file_2.fq.gz -o ~/Assignment1
+	 
+#Assessing quality of the two files	
+	echo "-----------------------------------------------------------------------"
+#Pass Check: Per Base Sequence quality, per sequence quality score, per Ncontent and Adapter content	
+	qualityChecks[0]=$( head -2 ${file_1}_fastqc/summary.txt | tail -1 | cut -f1) 
+	qualityChecks[1]=$( head -4 ${file_1}_fastqc/summary.txt | tail -1 | cut -f1) 
+	qualityChecks[2]=$( head -7 ${file_1}_fastqc/summary.txt | tail -1 | cut -f1)
+	qualityChecks[3]=$( head -11 ${file_1}_fastqc/summary.txt | tail -1  | cut -f1)
+ 	qualityChecks[4]=$( head -2 ${file_2}_fastqc/summary.txt | tail -1 | cut -f1)
+        qualityChecks[5]=$( head -4 ${file_2}_fastqc/summary.txt | tail -1 | cut -f1)
+        qualityChecks[6]=$( head -7 ${file_2}_fastqc/summary.txt | tail -1 | cut -f1)
+        qualityChecks[7]=$( head -11 ${file_2}_fastqc/summary.txt | tail -1  | cut -f1)
+	
+	quality_fail=false
+	for i in "${qualityChecks[@]}"; do
+    		if [[ "PASS" != "$i" ]]
+			 then quality_fail=true
+        		      break
+    		fi	
+	done
+	
+	if [[ "$quality_fail" == false ]]
+		 then  echo "Quality Check Passed!"
+	else
+		echo "Quality check fail in on of the pairs!"
+		cat ${file_1}_fastqc/summary.txt
+		cat ${file_2}_fastqc/summary.txt
+	fi
+	echo "-----------------------------------------------------------------------"
+done < "$fqTable"
+
 #2. Asses quality
 #3. Alignment Using bowtie2
 #4. Generate counts data 
