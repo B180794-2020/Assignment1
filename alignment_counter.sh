c88a2e23f6b85673b23a10d04da2e5bde24bc177
B180794-2020 <email_not_provided>
2020-10-21 23:22:02 +0100

    alignment_counter with working quality check, assessing quality, alignment and conversion of alignment output into correct .bam format

--- alignment_counter.sh
+++ alignment_counter.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
-#TODO:
-#1. Quality check using fastqc
-#Constructing index genome
-#Pathing to list of sequence files  & looping rows containing each pair 
+
+#Assinging variables for easy pathing to list of sequence files  & looping rows containing each pair 
 fqDir="/localdisk/data/BPSM/Assignment1/fastq"
 fqTable="/localdisk/data/BPSM/Assignment1/fastq/fqfiles"
 
-#building index
+#Constructing indexed genome of Tryposoma bruncei bruncei for bowtie2 alignment and storing it in working directory
 gunzip -c /localdisk/data/BPSM/Assignment1/Tbb_genome/Tb927_genome.fasta.gz > ~/Assignment1/TbbGenome.fasta
 bowtie2-build --threads 2 TbbGenome.fasta T_brucei
 
+#Main loop to conduct quality check alignment and output on each sample one at a time
 while IFS= read -r  line; do
-#extracting file names of each pair in pair-sequence alignment & running quality check using fastqc
+
+#identifying filenames from fqTable for each pair in the pair-sequence alignment & running quality check using fastqc
 	file_1=$(echo $line | cut -d" " -f3 | cut -d "." -f1)
 	file_2=$(echo $line | cut -d" " -f4 | cut -d "." -f1)
-	fastqc --extract  -t 2  $fqDir/$file_1.fq.gz $fqDir/$file_2.fq.gz -o ~/Assignment1
-	 
-#2. Assessing quality of the two files	
-	echo "-----------------------------------------------------------------------"
-#Pass Check: Per Base Sequence quality, per sequence quality score, per Ncontent and Adapter content	
+	fastqc --extract  -t 2  $fqDir/$file_1.fq.gz $fqDir/$file_2.fq.gz -o ~/Assignment1	
+
+#Pass Check based on fastqc summary report: Per Base Sequence quality, per sequence quality score, per N content and Adapter content	
 	qualityChecks[0]=$( head -2 ${file_1}_fastqc/summary.txt | tail -1 | cut -f1) 
 	qualityChecks[1]=$( head -4 ${file_1}_fastqc/summary.txt | tail -1 | cut -f1) 
 	qualityChecks[2]=$( head -7 ${file_1}_fastqc/summary.txt | tail -1 | cut -f1)
@@ -27,7 +25,8 @@ while IFS= read -r  line; do
         qualityChecks[5]=$( head -4 ${file_2}_fastqc/summary.txt | tail -1 | cut -f1)
         qualityChecks[6]=$( head -7 ${file_2}_fastqc/summary.txt | tail -1 | cut -f1)
         qualityChecks[7]=$( head -11 ${file_2}_fastqc/summary.txt | tail -1  | cut -f1)
-	
+
+#Check that all tests are passed	
 	quality_fail=false
 	for i in "${qualityChecks[@]}"; do
     		if [[ "PASS" != "$i" ]]
@@ -35,17 +34,25 @@ while IFS= read -r  line; do
         		      break
     		fi	
 	done
-	
+#If the quality of BOTH files are good --> continue to bowtie alignment 
 	if [[ "$quality_fail" == false ]]
-		 then  echo "Quality Check Passed!"
+		then
+		echo "-----------------------"
+		echo "Quality Check Passed!"
+		echo "-----------------------"
+
+#Bowtie alignment using previously indexed genome, converting bowtie sam output to bam using samtools, sorting and indexing output
+		bowtie2 --threads 4 -x T_brucei -1 $fqDir/$file_1.fq.gz -2 $fqDir/$file_2.fq.gz | samtools view -bS - > $file_1.bam
+        	samtools sort $file_1.bam -o $file_1.sorted.bam
+        	samtools index $file_1.sorted.bam
+
+
 	else
 		echo "Quality check fail in on of the pairs!"
 		cat ${file_1}_fastqc/summary.txt
 		cat ${file_2}_fastqc/summary.txt
-	f
+	fi
 	echo "-----------------------------------------------------------------------"
-#3. Bowtie2 alignment
-	 bowtie2 --threads 2 -x T_brucei -1 $fqDir/$file_1.fq.gz -2 $fqDir/$file_2.fq.gz -S $file_1.sam
 
 done < "$fqTable"
 
