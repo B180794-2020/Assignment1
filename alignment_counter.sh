e0a3c09b2e34adc0d6304fea94c9328612392969
B180794-2020 <email_not_provided>
2020-10-21 12:24:21 +0100

    updated alignment_counter.sh, runs fastqc on each seq

--- alignment_counter.sh
+++ alignment_counter.sh
@@ -3,13 +3,13 @@
 #1. Quality check using fastqc
 
 #Pathing to list of sequence files  & looping rows containing each pair 
+fqDir="/localdisk/data/BPSM/Assignment1/fastq"
 fqTable="/localdisk/data/BPSM/Assignment1/fastq/fqfiles"
 while IFS= read -r  line
 	do
 	seq_1=$(echo $line | cut -d" " -f3)
 	seq_2=$(echo $line | cut -d" " -f4)
-	echo "$seq_1"
-	echo "$seq_2"
+	fastqc -t 2  $fqDir/$seq_1 $fqDir/$seq_2 -o ~/Assignment1
 	done < "$fqTable"
 #2. Asses quality
 #3. Alignment Using bowtie2
